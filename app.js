168f7877dcf2a59681b6dbfbec8c59716060b76c
Géry Debongnie <ged@odoo.com>
2020-09-02 15:58:49 +0200

    [STEP 3] Displaying tasks

--- app.js
+++ app.js
@@ -3,11 +3,33 @@
     const { xml } = owl.tags;
     const { whenReady } = owl.utils;
     
-    // Owl Components
+    const TASKS = [
+        {
+          id: 1,
+          title: "buy milk",
+          isCompleted: true,
+        },
+        {
+          id: 2,
+          title: "clean house",
+          isCompleted: false,
+        },
+      ];
+
     class App extends Component {
-      static template = xml`<div>todo app</div>`;
+        static template = xml/* xml */ `
+          <div class="task-list">
+              <t t-foreach="tasks" t-as="task" t-key="task.id">
+                  <div class="task">
+                      <input type="checkbox" t-att-checked="task.isCompleted"/>
+                      <span><t t-esc="task.title"/></span>
+                  </div>
+              </t>
+          </div>`;
+      
+        tasks = TASKS;
     }
-    
+
     // Setup code
     function setup() {
       const app = new App();
