54248f1e6844d7fea1fed35f76f6d8cf54c5d9e9
Géry Debongnie <ged@odoo.com>
2020-09-02 15:29:55 +0200

    [STEP 2] adding the first component

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 (function () {
-    console.log("hello owl", owl.__info__.version);
-  })();
+    const { Component } = owl;
+    const { xml } = owl.tags;
+    const { whenReady } = owl.utils;
+    
+    // Owl Components
+    class App extends Component {
+      static template = xml`<div>todo app</div>`;
+    }
+    
+    // Setup code
+    function setup() {
+      const app = new App();
+      app.mount(document.body);
+    }
+    
+    whenReady(setup);
+})();
